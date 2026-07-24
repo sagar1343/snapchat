5afd08abc56356a88c7efcc397710a2fe2ff7389
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2026-07-24 10:53:34 +0530

    created login, logout & register view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import login, logout
 
 
 # Create your views here.
+@require_http_methods(["GET", "POST"])
+def register_view(request):
+    if request.user.is_authenticated():
+        return redirect("home")
+
+    form = forms.RegisterForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect("home")
+    return render(request, "accounts/register.html", {"form": form})
+
+
+@require_http_methods(["GET", "POST"])
+def login_view(request):
+    if request.user.is_authenticated():
+        return redirect("home")
+
+    form = forms.LoginForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        login(request, form.get_user())
+        return redirect("home")
+    return render(request, "accounts/login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
 def home(request):
     return render(request, "pages/chat.html")
